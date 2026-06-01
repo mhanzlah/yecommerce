841a0a34dd90d7bc31cdc7808381740d9dff3442
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-06-01 17:59:00 +0500

    add allowed origins log

--- apps/server/src/app.js
+++ apps/server/src/app.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import express from 'express';
+import express from "express";
 
 import authRouter from "./routes/auth.routes.js";
 import categoryRouter from "./routes/category.routes.js";
@@ -9,23 +9,26 @@ import cartRouter from "./routes/cart.routes.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import config from "./config/config.js";
 
+console.log(config.ALLOWED_ORIGINS);
 
 const app = express();
 
 app.set("trust proxy", 1);
 
-app.use(cors({
+app.use(
+  cors({
     origin: config.ALLOWED_ORIGINS,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-}));
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Yecommerce service is up and running.');
-})
+app.get("/", (req, res) => {
+  res.send("Yecommerce service is up and running.");
+});
 
 app.use("/api/auth", authRouter);
 
